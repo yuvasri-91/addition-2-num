c69f014a6d8808d7c88e5dedb2348e1f909afaf6
yuva <yuva@gmail.com>
2025-01-06 05:37:37 +0530

    changes added

--- src/test/java/jar/AppTest.java
+++ src/test/java/jar/AppTest.java
@@ -15,11 +15,13 @@ public class AppTest {
     @Test
     public static void testAddition() {
         int result1 = App.add(10,20);
-        assertEquals(13, result1, "10 + 20 should equal 30");
+        assertEquals(30, result1, "10 + 20 should equal 30");
         int result2 = App.add(20,30);
-        assertEquals(28, result2, "20 + 30 should equal 50");
-        int result3 = App.add(-5,19);
+        assertEquals(50, result2, "20 + 30 should equal 50");
+        int result3 = App.add(number1:-5,19);
         assertEquals(14, result3, "-5 + 19 should equal 14");
+        int result4 = App.add(0,number2:0);
+        assertEquals(0, result3, "0 + 0 should equal 0");
     }
-    
+
 }
